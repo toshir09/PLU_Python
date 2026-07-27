cda9801593207bacf157c54e82cb02c5c97eec9c
Toshir09 <nanitoshir@gmail.com>
2026-07-27 10:41:03 +0530

    hi

--- Test2/q3t3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the number of students: "))
-marks = []
-print("Enter the marks:")
-for i in range(n):
-    marks.append(int(input()))
-marks.sort()
-print("Marks in ascending order:")
-for mark in marks:
-    print(mark, end=" ")
--- Test2/q4t3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the number of participants: "))
-timings = []
-print("Enter the timings (in seconds):")
-for i in range(n):
-    timings.append(float(input()))
-timings.sort()
-print("Ranked Timings (Fastest to Slowest):")
-for time in timings:
-    print(time, end=" ")
